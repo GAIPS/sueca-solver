13272e7c071e1390afe19a8772cdd2cebd2df3b4
PipzCorreiaz <filipaincorreia@gmail.com>
2015-06-23 19:24:39 +0100

    Add files to compute the linear regressions.

---
+++ results/linearRegressionTeam02.m
@@ -0,0 +1,14 @@
+log = load('log1.txt');
+pi_0 = log(:,1);
+pi_2 = log(:,3);
+ti_0 = log(:,5);
+ti_2 = log(:,7);
+Ai_0 = log(:,9);
+Ai_2 = log(:,11);
+Si_0 = log(:,13);
+Si_2 = log(:,15);
+pf_02 = log(:,17);
+tbl = table(pi_0, pi_2, ti_0, ti_2, Ai_0, Ai_2, Si_0, Si_2, pf_02,'VariableNames',{'pi_0','pi_2','ti_0','ti_2','Ai_0','Ai_2','Si_0','Si_2','pf_02'});
+mdl = fitlm(tbl,'pf_02 ~ pi_0 + pi_2 + ti_0 + ti_2 + Ai_0 + Ai_2 + Si_0 + Si_2')
+result = table2array(mdl.Coefficients);
+save('LinRegResultTeam02.txt', 'result', '-ascii');
---
+++ results/linearRegressionTeam13.m
@@ -0,0 +1,14 @@
+log = load('log1.txt');
+pi_1 = log(:,2);
+pi_3 = log(:,4);
+ti_1 = log(:,6);
+ti_3 = log(:,8);
+Ai_1 = log(:,10);
+Ai_3 = log(:,12);
+Si_1 = log(:,14);
+Si_3 = log(:,16);
+pf_13 = 120 - log(:,17);
+tbl = table(pi_1, pi_3, ti_1, ti_3, Ai_1, Ai_3, Si_1, Si_3, pf_13,'VariableNames',{'pi_1','pi_3','ti_1','ti_3','Ai_1','Ai_3','Si_1','Si_3','pf_13'});
+mdl = fitlm(tbl,'pf_13 ~ pi_1 + pi_3 + ti_1 + ti_3 + Ai_1 + Ai_3 + Si_1 + Si_3')
+result = table2array(mdl.Coefficients);
+save('LinRegResultTeam13.txt', 'result', '-ascii');
