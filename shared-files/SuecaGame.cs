7911cd9395c10875d3bd0ab1f97c178e791bc492
PipzCorreiaz <filipacorreia@tecnico.ulisboa.pt>
2016-01-28 13:32:40 +0000

    Change filenames

---
+++ shared-files/SuecaGame.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuecaSolver
+{
+    public class SuecaGame
+    {
+        private int trump;
+        private List<Trick> tricks;
+        private int firstPlayer;
+
+        public SuecaGame(int trumpSuit, int firstPlayerId)
+        {
+            trump = trumpSuit;
+            tricks = new List<Trick>(10);
+            tricks.Add(new Trick(trumpSuit));
+            firstPlayer = firstPlayerId;
+        }
+
+        public void PlayCard(int playerId, int card)
+        {
+            if (tricks.Count - 1 < 0)
+            {
+                Console.WriteLine("SuecaGame.PlayerCard >> Negative index");
+                System.Environment.Exit(1);
+            }
+
+            Trick currentTrick = tricks[tricks.Count - 1];
+            if (currentTrick.IsFull())
+            {
+                tricks.Add(new Trick(trump));
+                currentTrick = tricks[tricks.Count - 1];
+            }
+            currentTrick.ApplyMove(new Move(playerId, card));
+        }
+
+
+        public int GetNextPlayerId()
+        {
+            if (tricks.Count - 1 < 0)
+            {
+                return firstPlayer;
+            }
+            
+            return tricks[tricks.Count - 1].GetNextPlayerId();
+        }
+
+        public int[] GetGamePoints()
+        {
+            int firstTeamPoints = 0, secondTeamPoints = 0;
+            foreach (Trick t in tricks)
+            {
+                int[] winnerAndPoints = t.GetTrickWinnerAndPoints();
+                if (winnerAndPoints[0] == 0 || winnerAndPoints[0] == 2)
+                {
+                    firstTeamPoints += winnerAndPoints[1];
+                }
+                else
+                {
+                    secondTeamPoints += winnerAndPoints[1];
+                }
+            }
+
+            return new int[] { firstTeamPoints, secondTeamPoints};
+        }
+
+        public void PrintLastTrick()
+        {
+            if (tricks.Count > 0 && tricks[0].IsFull())
+            {
+                Console.WriteLine("Last trick:");
+                Trick currentTrick = tricks[tricks.Count - 1];
+                if (currentTrick.IsFull())
+                {
+                    currentTrick.PrintTrick();
+                }
+                else
+                {
+                    tricks[tricks.Count - 2].PrintTrick();
+                }
+                Console.WriteLine("");
+            }
+        }
+
+
+        public void PrintCurrentTrick()
+        {
+            Console.WriteLine("Current trick:");
+            Trick currentTrick = tricks[tricks.Count - 1];
+            if (!currentTrick.IsFull())
+            {
+                currentTrick.PrintTrick();
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ shared-files/TimeLimitedPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+
+namespace SuecaSolver
+{
+    public class SmartestPlayer : ArtificialPlayer
+    {
+        //private int _idDiff;
+        private int _handSize;
+        //private PIMC pimc;
+        private InformationSet infoSet;
+
+
+        public SmartestPlayer(int id, List<int> initialHand, int trumpSuit)
+            : base(id)
+        {
+            //_idDiff = 0 - id;
+            _handSize = initialHand.Count;
+            //pimc = new PIMC();
+            infoSet = new InformationSet(id, initialHand, trumpSuit);
+        }
+
+        override public void AddPlay(int playerID, int card)
+        {
+            //int playerIdForMe = playerID + _idDiff;
+            //if (playerIdForMe < 0)
+            //{
+            //    playerIdForMe += 4;
+            //}
+            infoSet.AddPlay(playerID, card);
+        }
+
+
+        override public int Play()
+        {
+            int chosenCard;
+
+            if (_handSize > 10)
+            {
+                chosenCard = infoSet.RuleBasedDecision();
+            }
+            else
+            {
+                chosenCard = PIMC.ExecuteWithTimeLimit(_id, infoSet, new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
+            }
+
+            //infoSet.AddMyPlay(chosenCard);
+            _handSize--;
+            return chosenCard;
+        }
+    }
+}
