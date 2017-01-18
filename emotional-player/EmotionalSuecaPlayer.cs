bf53498f8baf43529f78acd76f22323919155f6b
Filipa Correia <filipacorreia@tecnico.ulisboa.pt>
2017-01-18 14:59:12 +0000

    Add new EmotionalSuecaPlayer

---
+++ emotional-player/EmotionalSuecaPlayer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thalamus;
+using SuecaSolver;
+using SuecaMessages;
+
+namespace emotionalPlayer
+{
+    public interface ISuecaPublisher : IThalamusPublisher, ISuecaActions { }
+
+
+    class EmotionalSuecaPlayer : ThalamusClient, ISuecaPerceptions
+    {
+        private class SuecaPublisher : ISuecaPublisher
+        {
+            dynamic publisher;
+
+            public SuecaPublisher(dynamic publisher)
+            {
+                this.publisher = publisher;
+            }
+
+            public void Play(int id, string card)
+            {
+                this.publisher.Play(id, card);
+            }
+        }
+
+
+        private ISuecaPublisher suecaPublisher;
+
+        public EmotionalSuecaPlayer(string clientName, string charactersNames = "")
+            : base(clientName, charactersNames)
+        {
+            SetPublisher<ISuecaPublisher>();
+            suecaPublisher = new SuecaPublisher(Publisher);
+        }
+
+        public void Cut(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deal(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GameEnd(int team0Score, int team1Score)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GameStart(int gameId, int playerId, int teamId, string trumpCard, int trumpCardPlayer, string[] cards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextPlayer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play(int id, string card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveRobotCards(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Renounce(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetTrick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SessionEnd(int sessionId, int team0Score, int team1Score)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SessionStart(int sessionId, int numGames, int[] agentsIds, int shouldGreet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shuffle(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrickEnd(int winnerId, int trickPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrumpCard(string trumpCard, int trumpCardPlayer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ emotional-player/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emotionalPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmotionalSuecaPlayer sp;
+            string client = "";
+            string character = "";
+            if (args.Length == 2)
+            {
+                client = args[0];
+                character = args[1];
+            }
+            else if (args.Length == 1)
+            {
+                client = "IA";
+                character = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Unspecified client name and character for Thalamus.");
+            }
+
+            if (client != "" && character != "")
+            {
+                sp = new EmotionalSuecaPlayer(client, character);
+            }
+            else
+            {
+                sp = new EmotionalSuecaPlayer("IA");
+            }
+            Console.ReadLine();
+            sp.Dispose();
+        }
+    }
+}
