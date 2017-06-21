1ba688d73df7d259663889f4943cd1cd90e045da
Filipa Correia <filipacorreia@tecnico.ulisboa.pt>
2017-06-21 10:27:46 +0100

    Get model weights

--- state-inference/state_inference.py
+++ state-inference/state_inference.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os.path
+import math
+import numpy as np
+from sklearn import linear_model
 
 def main():
     processedhandsFile = Path('..\sueca-logs\processedHands.txt')
@@ -25,11 +28,27 @@ def main():
         print('ProcessedHands file not found.')
         return
 
-    doSomething(abstractHands)
+    calculateFeaturesWeights(abstractHands)
 
-def doSomething(hands):
-    for key, value in hands:
-        print(key)
+def calculateFeaturesWeights(hands):
+    
+    n_samples = len(hands.keys())
+    n_features = 5
+    X = np.ones((n_samples, n_features))
+    y = np.ones(n_samples)
+    i = 0
+    for key in hands:
+        y[i] = math.log(hands[key])
+        parsed = key.split('\t')
+        for j in range(n_features):
+            if j < n_features - 1:
+                X[i][j] = float(parsed[j])
+        i += 1
+
+
+    clf = linear_model.SGDRegressor()
+    clf.fit(X, y)
+    print(clf.coef_)
 
 
 if __name__ == "__main__":
